2baca6671dc65a0b2f0c24253e3cd96a7781738b
Konstantin Ivaschenko <stdcion@gmail.com>
2022-08-03 17:08:51 +0300

    Remove obsolete extension

--- dxfeed-graal-native/build_win.cmd
+++ dxfeed-graal-native/build_win.cmd
@@ -1,3 +1,3 @@
 cmake --preset="conf-win-vs" -DCMAKE_BUILD_TYPE=Release .
 cmake --build --preset="build-win-vs" --config=Release
-ctest.exe --test-dir build/conf-win-vs -C Release --extra-verbose
+ctest --test-dir build/conf-win-vs -C Release --extra-verbose
