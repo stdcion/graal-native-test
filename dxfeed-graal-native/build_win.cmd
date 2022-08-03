b6f335642da4a886d2084a694bb20f9df8326063
Konstantin Ivaschenko <stdcion@gmail.com>
2022-08-03 16:58:07 +0300

    Add test settings

--- dxfeed-graal-native/build_win.cmd
+++ dxfeed-graal-native/build_win.cmd
@@ -1,2 +1,3 @@
 cmake --preset="conf-win-vs" -DCMAKE_BUILD_TYPE=Release .
 cmake --build --preset="build-win-vs" --config=Release
+ctest.exe --test-dir build/conf-win-vs -C Release --extra-verbose
---
+++ dxfeed-graal-native/tests/test_foo/main.cpp
@@ -0,0 +1,10 @@
+#include <catch.hpp>
+
+TEST_CASE("Catch test", "[Catch2]") {
+  REQUIRE(true);
+}
+
+TEST_CASE("Catch test1", "[Catch2]") {
+  REQUIRE(true);
+}
+
